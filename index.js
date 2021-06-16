355dd4dcfb358edc42ca1014a94609f2dd5ed610
aryaaniyan123 <aryaaniyan108@gmail.com>
2021-06-16 14:07:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server,{ cors:{ origin: "#"}})
+
+
+app.set('view engine', 'ejs')
+
+
+app.get('/home', (req,res)  => {
+    res.render('home')
+
+})
+
+server.listen(3000, () => {
+    console.log('server running...')
+});
+io.on( 'connection' , (socket) => {
+    console.log("user connected: " + socket.id);
+
+    socket.on("message", (data) => {
+        socket.broadcast.emit("message", data)
+    });
+  
+});
